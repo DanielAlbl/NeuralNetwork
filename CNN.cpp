3cb2ce6a38d5e559ac75460a959652134315c468
Dan Albl <danalbl1997@gmail.com>
2021-07-17 16:05:15 -0600

    Created forward function for cnn

--- CNN.cpp
+++ CNN.cpp
@@ -1,1 +1,17 @@
 #include "CNN.h"
+
+void CNN::forward(tensor3& x) {
+	int j = 0;
+	V[0] = x;
+
+	for (int i = 0; i < layers.size(); i++) {
+		if (layers[i].size() == 3) {
+			CNV(V[i + 1], V[i], W[j]);
+			ADD(V[i + 1], V[i + 1], B[j]);
+			ACT(V[i + 1], V[i + 1]);
+			j++;
+		}
+		else
+			MPL(V[i + 1], V[i], layers[i][0], layers[i][1]);
+	}
+}
--- CNN.h
+++ CNN.h
@@ -1,8 +1,20 @@
 #pragma once
-#include "tensor.h"
+#include "Tensor.h"
+#include "Classifier.h"
 
 class CNN {
-	tensor4 W;
-	tensor2 B;
+	vector<vector<int>> layers; // stores dimensionts of conv and pool layers
+
+	tensor5 W; // weights
+	tensor2 B; // biases
+
+	tensor4 V; // temp storage of tensors 
+
+	tensor5 dW; // derivative of weights
+	tensor2 dB; // derivative of biases
+
+	Classifier& C; // fully connected network at the end
+
 public:
+	void forward(tensor3& x);
 };
